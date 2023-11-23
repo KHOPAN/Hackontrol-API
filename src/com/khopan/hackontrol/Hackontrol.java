fbd5826ed7374d9c70a9adcc6b121cb57a1e9f28
KHOPAN <93379212+KHOPAN@users.noreply.github.com>
2023-11-23 22:16:59 +0700

    Setup JDA and Exceptions

--- src/com/khopan/hackontrol/Hackontrol.java
+++ src/com/khopan/hackontrol/Hackontrol.java
@@ -1,7 +1,23 @@
 package com.khopan.hackontrol;
 
-public class Hackontrol {
-	public Hackontrol() {
+import com.khopan.hackontrol.exception.AwaitReadyException;
 
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+
+public class Hackontrol {
+	private final JDA bot;
+
+	public Hackontrol() {
+		this.bot = JDABuilder.createDefault(Token.BOT_TOKEN)
+				.enableIntents(GatewayIntent.GUILD_MESSAGES)
+				.build();
+
+		try {
+			this.bot.awaitReady();
+		} catch(InterruptedException | IllegalStateException Errors) {
+			throw new AwaitReadyException("Error while awaitReady()", Errors);
+		}
 	}
 }
