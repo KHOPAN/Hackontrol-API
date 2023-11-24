007e166afc2d91809fabd5bed6019eae200e36a5
KHOPAN <93379212+KHOPAN@users.noreply.github.com>
2023-11-24 21:12:43 +0700

    Test Target Listener

--- src/test/Main.java
+++ src/test/Main.java
@@ -1,9 +1,24 @@
 package test;
 
 import com.khopan.hackontrol.Hackontrol;
+import com.khopan.hackontrol.Target;
+import com.khopan.hackontrol.target.TargetListener;
 
 public class Main {
 	public static void main(String[] args) {
 		Hackontrol hackontrol = new Hackontrol();
+		hackontrol.setTargetListener(new Listener());
+	}
+
+	private static class Listener implements TargetListener {
+		@Override
+		public void onTargetConnected(Target target) {
+			System.out.println("Connected: " + target.getMachineIdentifier());
+		}
+
+		@Override
+		public void onTargetDisconnected(Target target) {
+			System.out.println("Disconnected: " + target.getMachineIdentifier());
+		}
 	}
 }
