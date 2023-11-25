1c701b44b4abb92ed85a1ace35cb67cb23b4870c
KHOPAN <93379212+KHOPAN@users.noreply.github.com>
2023-11-25 22:30:53 +0700

    Test Command API

--- src/test/Main.java
+++ src/test/Main.java
@@ -1,10 +1,5 @@
 package test;
 
-import java.awt.image.BufferedImage;
-import java.io.File;
-
-import javax.imageio.ImageIO;
-
 import com.khopan.hackontrol.Hackontrol;
 import com.khopan.hackontrol.Target;
 import com.khopan.hackontrol.target.TargetListener;
@@ -19,15 +14,7 @@ public class Main {
 		@Override
 		public void onTargetConnected(Target target) {
 			System.out.println("Connected: " + target.getMachineIdentifier());
-			System.out.println("Taking...");
-			BufferedImage image = target.screenshot();
-			System.out.println("Taken!");
-
-			try {
-				ImageIO.write(image, "png", new File("C:\\screenshot.png"));
-			} catch(Throwable Errors) {
-				Errors.printStackTrace();
-			}
+			System.out.println(target.command("whoami"));
 		}
 
 		@Override
